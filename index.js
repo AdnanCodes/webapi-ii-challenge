bae5a81080bebd9166988711657a7a83bfda470b
Adnan <adnan.chowdhury@outlook.com>
2019-09-03 17:11:44 -0500

    Added PUT endpoint for updating POSTS

--- index.js
+++ index.js
@@ -123,5 +123,30 @@ server.delete("/api/posts/:id", (req, res) => {
     });
 });
 
+//Updating a post
+server.put("/api/posts/:id", (req, res) => {
+  const { title, contents } = req.body;
+  if (title || contents) {
+    DataBase.update(req.params.id, req.body)
+      .then(post => {
+        if (post) {
+          res.status(200).json(post);
+        } else {
+          res.status(404).json({
+            message: "The post with the specified ID does not exist."
+          });
+        }
+      })
+      .catch(() =>
+        res
+          .status(500)
+          .json({ error: "The post information could not be modified." })
+      );
+  } else {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  }
+});
 const port = 3000;
 server.listen(port, () => console.log(`\n API is running on Port ${port}\n`));
