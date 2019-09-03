62f77f236f82aef7a4e6102151dab2bc1c1316c1
Adnan <adnan.chowdhury@outlook.com>
2019-09-03 15:03:39 -0500

    Endpoint for creating a new post on server

--- index.js
+++ index.js
@@ -6,5 +6,29 @@ server.use(express.json());
 
 //Build Endpoints
 
+//POST ENDPOINT for posts
+
+server.post("/api/posts", (req, res) => {
+  const { title, contents } = req.body;
+
+  if (title || contents) {
+    DataBase.insert(req.body)
+      .then(post => res.status(201).json(post))
+      .catch(() => {
+        res
+          .status(500)
+          .json({
+            error: "There was an error while saving the post to the database"
+          });
+      });
+  } else {
+    res
+      .status(400)
+      .json({
+        errorMessage: "Please provide title and contents for the post."
+      });
+  }
+});
+
 const port = 3000;
 server.listen(port, () => console.log(`\n API is running on Port ${port}\n`));
